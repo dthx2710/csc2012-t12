6eb2e4cba8ab950354ce24d9cbde93b8cca6d3c6
dthx2710 <dthx2710@hotmail.com>
2023-03-03 14:49:01 +0800

    chore: init node grpc server

--- user-service/user.js
+++ user-service/user.js
@@ -1,10 +1,17 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
-const packageDefinition = protoLoader.loadSync('path/to/your/proto/file.proto');
+const packageDefinition = protoLoader.loadSync('proto/user-service.proto',
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
+  
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const userProto = protoDescriptor.UserService;
-
+const userProto = protoDescriptor.User;
 const client = new userProto('localhost:50051', grpc.credentials.createInsecure());
 
 function getUser(userId) {
