6a5bd7fede21948f29eea8a87989bbb9db8da9dd
Dolicon <dthx2710@hotmail.com>
2023-03-09 19:32:58 +0800

    feat: working gRPC Go Gateway (user-service)

--- gateway/src/main.go
+++ gateway/src/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	gw "csc2012-t12/gateway/proto"
-	"log"
-	"net"
+	"flag"
 	"net/http"
+	"strings"
 
+	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type UserServer struct {
@@ -29,26 +31,80 @@ func (s *UserServer) GetInfo(ctx context.Context, req *gw.InfoRequest) (*gw.Info
 	return &gw.InfoResponse{Username: user.Username, Name: user.Name, Points: user.Points}, nil
 }
 
+// service endpoints
+var (
+	userEndpoint = flag.String("user_endpoint", "localhost:50051", "endpoint of User Service")
+	// imageendpoint = flag.String("image_endpoint", "localhost:50052", "endpoint of Image Service")
+)
+
+func newGateway(ctx context.Context, opts ...runtime.ServeMuxOption) (http.Handler, error) {
+	mux := runtime.NewServeMux(opts...)
+	dialOpts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := gw.RegisterUserHandlerFromEndpoint(ctx, mux, *userEndpoint, dialOpts)
+	if err != nil {
+		return nil, err
+	}
+	return mux, nil
+}
+
+// allowCORS allows Cross Origin Resoruce Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	glog.Infof("preflight request for %s", r.URL.Path)
+	return
+}
+
+// Run starts a HTTP server and blocks forever if successful.
+func Run(address string, opts ...runtime.ServeMuxOption) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := http.NewServeMux()
+
+	gw, err := newGateway(ctx, opts...)
+	if err != nil {
+		return err
+	}
+	mux.Handle("/", gw)
+
+	return http.ListenAndServe(address, allowCORS(mux))
+}
+
 func main() {
-	go func() {
-		// mux
-		mux := runtime.NewServeMux()
-		// register
-		gw.RegisterUserHandlerServer(context.Background(), mux, &UserServer{})
-		// http server
-		log.Fatalln(http.ListenAndServe(":8081", mux))
-	}()
+	flag.Parse()
+	defer glog.Flush()
 
-	listen, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+	if err := Run(":8080"); err != nil {
+		glog.Fatal(err)
 	}
 
-	grpcServer := grpc.NewServer()
-	gw.RegisterUserServer(grpcServer, &UserServer{})
-	err = grpcServer.Serve(listen)
-	if err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	// listen, err := net.Listen("tcp", ":8080")
+	// if err != nil {
+	// 	log.Fatalf("did not connect: %v", err)
+	// }
 
+	// grpcServer := grpc.NewServer()
+	// gw.RegisterUserServer(grpcServer, &UserServer{})
+	// err = grpcServer.Serve(listen)
+	// if err != nil {
+	// 	log.Fatalf("failed to serve: %v", err)
+	// }
 }
