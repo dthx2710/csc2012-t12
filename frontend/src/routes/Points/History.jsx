bd3b5941a218862d9420033068cb7772385a456c
zyferis <derickkwok13@gmail.com>
2023-03-12 18:23:37 +0800

    added points pages

--- frontend/src/routes/Points/History.jsx
+++ frontend/src/routes/Points/History.jsx
@@ -1,8 +1,105 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarLink,
+  MDBNavbarBrand,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+  MDBCardImage,
+  MDBCardFooter,
+  MDBBadge,
+  MDBListGroup,
+  MDBListGroupItem
+
+}
+  from 'mdb-react-ui-kit';
+
 function History() {
-    return (
-      <h4>Tab 3 content</h4>
-    );
-  }
-  
-  export default History;
-  
+  return (
+    <>
+    <h2>History</h2>
+    <MDBListGroup light style={{ minWidth: '18rem', "text-align": "left"}}>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold' >Plastic Bottle</div>10 Feb 2023 10:35 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Plastic Bottle</div>10 Feb 2023 10:37 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Alluminium Can</div>11 Feb 2023 15:43 
+        </div>
+        <MDBBadge pill light>
+          +75 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Plastic Bottle</div>2 Mar 2023 12:25 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Plastic Bottle</div>2 Mar 2023 12:25 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Plastic Bottle</div>2 Mar 2023 12:25 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Plastic Bottle</div>2 Mar 2023 12:25 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+      <MDBListGroupItem className='d-flex justify-content-between align-items-start' style={{padding: "1rem 0.5rem"}}>
+        <div className='ms-2 me-auto'>
+          <div className='fw-bold'>Plastic Bottle</div>2 Mar 2023 12:25 
+        </div>
+        <MDBBadge pill light>
+          +125 points
+        </MDBBadge>
+      </MDBListGroupItem>
+    </MDBListGroup></>
+  );
+}
+
+export default History;
--- frontend/src/routes/Points/Rewards.jsx
+++ frontend/src/routes/Points/Rewards.jsx
@@ -1,8 +1,84 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarLink,
+  MDBNavbarBrand,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+  MDBCardImage,
+  MDBCardFooter,
+  MDBBadge,
+  MDBListGroup,
+  MDBListGroupItem
+
+}
+  from 'mdb-react-ui-kit';
+
 function Rewards() {
-    return (
-      <h4>Tab 2 content</h4>
-    );
-  }
-  
-  export default Rewards;
-  
+  return (
+    <MDBRow className='mt-5'>
+      <MDBCol>
+        <h2>My Rewards</h2>
+
+        <MDBCard
+          className='bg-white  my-5 mx-auto'
+          style={{ borderRadius: '1rem', maxWidth: '500px' }}>
+          <MDBCardBody className='w-100 d-flex flex-column'>
+            <MDBCardImage src='/Grab.svg' position='top' alt='...' />
+            <MDBCardTitle>Grab</MDBCardTitle>
+            <MDBCardText>
+              $10 e-Voucher
+            </MDBCardText>
+            {/* <MDBBtn>1000 points</MDBBtn> */}
+          </MDBCardBody>
+          <MDBCardFooter className='text-muted'>5a384bf76abd08ec77c</MDBCardFooter>
+        </MDBCard>
+
+        <MDBCard
+          className='bg-white  my-5 mx-auto'
+          style={{ borderRadius: '1rem', maxWidth: '500px' }}
+        >
+          <MDBCardBody className='w-100 d-flex flex-column'>
+            <MDBCardImage src='/Shopee.svg' position='top' alt='...' />
+            <MDBCardTitle>Shopee</MDBCardTitle>
+            <MDBCardText>
+              $10 e-Voucher
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter className='text-muted'>3cfd66d8913e0</MDBCardFooter>
+        </MDBCard>
+
+        <MDBCard
+          className='bg-white  my-5 mx-auto'
+          style={{ borderRadius: '1rem', maxWidth: '500px' }}
+        >
+          <MDBCardBody className='w-100 d-flex flex-column'>
+            <MDBCardImage src='/Lazada.svg' position='top' alt='...' />
+            <MDBCardTitle>Lazada</MDBCardTitle>
+            <MDBCardText>
+              $10 e-Voucher
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter className='text-muted'>76abd08ec77c3cfd66d89</MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+  );
+}
+
+export default Rewards;
